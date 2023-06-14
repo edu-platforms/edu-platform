297ce49f7a191c2f1164ac49e3f00151c5b4691b
Abdurahmon <87411075+AbdurahmonQobiljonov@users.noreply.github.com>
2023-06-14 16:08:39 +0500

    feat: profileSlice

--- src/libs/store/index.js
+++ src/libs/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth/features.js'
 import appReducer from '../slices/appSlice.js'
+import profileReducer from '../slices/profileSlice.js'
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     auth: authReducer,
+    profile: profileReducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) =>
